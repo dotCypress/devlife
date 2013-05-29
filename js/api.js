d5dbacfaa1ac0083e709eba9904e5c6efe58337e
Vitaly Domnikov <dotcypress@gmail.com>
2013-05-29 14:59:55 +0300

    Cleanup

--- js/api.js
+++ js/api.js
@@ -1,13 +1,9 @@
-angular.module('api', ['ngResource']).
-  factory('Feed', function($resource){
-    var api = $resource('http://developerslife.ru/:category/:page?jsonp=true&pageSize=10', {}, {
-      query: {method:'GET', params:{category:'latest', page: 0}, isArray:false}
-    });
+angular.module('api', []).
+  factory('Feed', function(){
 
     var Feed = {};
 
     Feed.get = function(mode, page, cb){
-
       $.getJSON("http://query.yahooapis.com/v1/public/yql",
         {
           q: "select * from json where url=\"http://developerslife.ru/" + mode + "/" + page + "?json=true&pageSize=5&salt=" + Math.random() + "\"",
